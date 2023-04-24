89047665d65bdc8028a6a3960f9fc8bcf1fa8394
leon <leonhudak@protonmail.com>
2023-04-24 19:34:26 +0200

    first commit

---
+++ src/transactions/schemas/transaction.schema.ts
@@ -0,0 +1,17 @@
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import { Document } from 'mongoose';
+
+export type TransactionDocument = Transaction & Document;
+
+@Schema()
+export class Transaction {
+  @Prop()
+  txID: number;
+
+  @Prop()
+  txHash: string;
+
+}
+
+
+export const TransactionSchema = SchemaFactory.createForClass(Transaction);
