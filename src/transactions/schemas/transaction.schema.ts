7caadf133dd94fac0cffb2d9c26dbd073e44a94c
leon <leonhudak@protonmail.com>
2023-04-24 19:35:39 +0200

    setup

--- src/transactions/schemas/transaction.schema.ts
+++ src/transactions/schemas/transaction.schema.ts
@@ -11,6 +11,8 @@ export class Transaction {
   @Prop()
   txHash: string;
 
+
+
 }
 
 
--- src/transactions/transaction.repository.ts
+++ src/transactions/transaction.repository.ts
@@ -1,4 +1,30 @@
 import { Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { Transaction, TransactionDocument } from './schemas/transaction.schema';
+import { FilterQuery, Model } from 'mongoose';
 
-@Injectable
-export class TransactionRepository{}
+@Injectable()
+export class TransactionRepository {
+  constructor(
+    @InjectModel(Transaction.name)
+    private transactionModel: Model<TransactionDocument>,
+  ) {
+  }
+
+  async findOne(
+    transactionFilterQuery: FilterQuery<Transaction>,
+  ): Promise<Transaction> {
+    return this.transactionModel.findOne(transactionFilterQuery);
+  }
+
+  async find(
+    transactionsFilterQuery: FilterQuery<Transaction>,
+  ): Promise<Transaction[]> {
+    return this.transactionModel.findOne(transactionsFilterQuery);
+  }
+
+  async create(tx: Transaction): Promise<Transaction> {
+    const newTX = new this.transactionModel(tx);
+    return newTX.save();
+  }
+}
